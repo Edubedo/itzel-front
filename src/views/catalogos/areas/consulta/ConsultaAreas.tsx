5db1bdd3ebbc232e1655443b8a4151c547e2417a
fer463-web <mzepeda22@ucol.mx>
2025-09-12 12:03:04 -0600

    Consultar area y formulario
    modificacion de llamada a tabla para consultar area

--- src/views/catalogos/areas/consulta/ConsultaAreas.tsx
+++ src/views/catalogos/areas/consulta/ConsultaAreas.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import AreaTables from '../../../../pages/Tables/AreaTables'
 
 function ConsultaAreas() {
   return (
-    <div>ConsultaAreas</div>
+    <AreaTables titleTable="Consulta de Áreas" />
   )
 }
 
-export default ConsultaAreas
+export default ConsultaAreas;
